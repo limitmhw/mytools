e0979ba5f161800c43100a14039ae04633b35de4
limitmhw <limitmhw@gmail.com>
2026-04-02 13:14:36 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -17,7 +17,7 @@ alias gc="git checkout"
 alias gd="git diff"
 alias glp="git log --pretty=\"%h %cd %an ==>%s\" --date=short"
 alias ll="ls -l --color"
-alias cld="claude --dangerously-skip-permissions"
+alias cld="IS_SANDBOX=1 claude --dangerously-skip-permissions"
 
 
 # claude --dangerously-skip-permissions
